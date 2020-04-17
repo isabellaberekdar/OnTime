eccf4a47340daaef7e37a94424f58a118a06f41a
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-04-16 20:43:26 -0400

    renamed folders

---
+++ store/utilities/events.js
@@ -0,0 +1,48 @@
+import axios from "axios"
+import events from "../../eventSampleData"
+
+// ACTION TYPES
+const GET_EVENTS = "GET_EVENTS"
+const EDIT_EVENT = "EDIT_EVENT"
+const CREATE_EVENT = "CREATE_EVENT"
+
+// ACTION CREATORS
+const getEvents = (eventsList) => {
+  return {
+    type: GET_EVENTS,
+    payload: eventsList,
+  }
+}
+
+// THUNK CREATORS
+export const getEventsThunk = (userId) => async (dispatch) => {
+  try {
+    /*     
+    const { data } = await axios.get(
+      `/events/${userId}`
+    );
+    */
+    // currently uses a hardcoded events list
+    dispatch(getEvents(events))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const initialState = {
+  events: events,
+}
+// REDUCER
+const eventsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_EVENTS:
+      return {
+        ...state,
+        events: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+export default eventsReducer
---
+++ store/utilities/users.js
@@ -0,0 +1,54 @@
+import axios from "axios"
+
+// ACTION TYPES
+const LOG_IN_USER = "LOG_IN_USER"
+const LOG_OUT_USER = "LOG_OUT_USER"
+const REGISTER_USER = "REGISTER_USER"
+
+// ACTION CREATORS
+const logInUser = (userInfo) => {
+  return {
+    type: LOG_IN_USER,
+    payload: userInfo,
+  }
+}
+
+// THUNK CREATORS
+export const logInUserThunk = (email, password) => async (dispatch) => {
+  try {
+    const credentials = {
+      email: email,
+      password: password,
+    }
+
+    const { data } = await axios.post(
+      "https://fair-hallway-265819.appspot.com/api/login",
+      credentials
+    )
+
+    // TODO: needs error handling for incorrect credentials or server-side issue
+    dispatch(logInUser(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// REDUCER
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case LOG_IN_USER:
+      const { firstName, lastName } = action.payload.userInfo
+      const { email, id } = action.payload.user
+      return {
+        ...state,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        id: id,
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
